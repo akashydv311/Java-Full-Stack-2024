0a67d8fd64dd1fdfe7027b8452f5b80d0233d87e
akashydv311 <akashydv311@gmail.com>
2024-07-10 20:55:37 +0530

    gfg pattern

---
+++ CoreJava/src/patternprograms/GFG.java
@@ -0,0 +1,53 @@
+package patternprograms;
+
+//Simple Pyramid Pattern
+//Flipped Simple Pyramid Pattern
+//Inverted Pyramid Pattern
+//Flipped Inverted Pyramid Pattern
+//Triangle Pattern
+//Inverted Triangle Pattern
+//Half Diamond Pattern
+//Flipped Half Diamon Pattern
+//Diamond Pattern
+//Hourglass Pattern
+//Number Pyramid Pattern
+//Numbers Pyramid Pattern without Reassigning
+//Continuous Number Pyramid Pattern after 180° Rotation
+//Palindrome Triangle Pattern
+//Alphabet Pyramid Pattern
+//Continuous Alphabet Pyramid Pattern
+
+public class GFG {
+	
+	public static void Pattern1() {
+//      Simple Pyramid Pattern
+//		****
+//		****
+//		****
+//		****
+		int row=4;
+		int col=4;
+		for(int i=0; i<row; i++) {
+			for(int j=0; j<col; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	public static void Pattern2() {
+//     Flipped Simple Pyramid Pattern
+		
+		int row=4;
+		int col=4;
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		//Pattern1();
+		Pattern2();
+	}
+}
