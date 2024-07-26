6d87d04ce377c1be7ae325c9a1ab62685ca2e6a0
akashydv311 <akashydv311@gmail.com>
2024-07-26 14:42:55 +0530

    TUF pattern added

--- CoreJava/src/patternprograms/TUF.java
+++ CoreJava/src/patternprograms/TUF.java
@@ -2,5 +2,10 @@ package patternprograms;
 
 //https://takeuforward.org/strivers-a2z-dsa-course/must-do-pattern-problems-before-starting-dsa/
 public class TUF {
-
+	public static void main(String[] args) {
+		
+		int x = 10;
+		
+		System.out.println(x);
+	}
 }
