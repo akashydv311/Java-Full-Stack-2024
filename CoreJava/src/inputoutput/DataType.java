87d48fe4654de341b9d540d2a54b72eec8730694
akashydv311 <akashydv311@gmail.com>
2024-08-04 14:59:06 +0530

    Datatype added

---
+++ CoreJava/src/inputoutput/DataType.java
@@ -0,0 +1,15 @@
+package inputoutput;
+
+public class DataType {
+	public static void main(String[] args) {
+		// Primitive Data Type
+//		Boolean data type
+//		byte data type
+//		char data type
+//		short data type
+//		int data type
+//		long data type
+//		float data type
+//		double data type
+	}
+}
